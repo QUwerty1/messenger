d5f5ab423fcd30e33eee8342e3ac3b44bf71df03
mrquw <mrquwerty@outlook.com>
2023-02-26 16:45:14 +0800

    initial commit

--- server/messenger/apps.py
+++ server/messenger/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CatalogConfig(AppConfig):
+class MessengerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'catalog'
+    name = 'messenger'
